ea67be924894908fbeed0ab6caf630a209915e78
Sumit Mistry <sumitmistry@live.com>
2024-09-29 10:46:49 -0400

    updates

--- src/main/java/springboot/ToDo/Autorun_justReference.java
+++ src/main/java/springboot/ToDo/Autorun_justReference.java
@@ -17,8 +17,9 @@ public class Autorun_justReference implements CommandLineRunner {
 
     @Override
     public void run(String[] strings) throws Exception{
-        System.out.println("\n\n                              in CMD line Runner " +
-                "                              AutoRunner: No methdos is defined here...jjust fyi....\n\n");
+        System.out.println("\n\n                              AutoRunner:   in CMD line Runner " +
+                "                              AutoRunner:   No methdos is defined here...jjust fyi...." +
+        "                              AutoRunner:   Any method defined here...will autorun ...fyi.... \n\n");
 
     }
 
--- src/main/java/springboot/ToDo/Controller/ToDo_Controller.java
+++ src/main/java/springboot/ToDo/Controller/ToDo_Controller.java
@@ -37,7 +37,7 @@ import java.util.function.Predicate;
 @Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 @SessionAttributes({"uid_email", "pass", "totally"})  // when you want to store a value in whole session, use this.
 // you have to pass this values from frontend variable standpoint, so it is <uid_email> not <usernr>
-// <usernr> is backend variable, this will nto work
+// <usernr> is backend variable, this will not work
 // "uid_email" is the frontend variable, passing this will be able to save as session. it will work..
 public class ToDo_Controller {
 
