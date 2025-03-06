6a5eeaaf9de63dd27c1b1aeba2a44a8c027f26a0
Sumit Mistry <sumitmistry@live.com>
2025-03-05 22:53:33 -0500

    restructuring_best_practices_in_rest_api_todo_controller

---
+++ src/main/java/springboot/ToDo/Controller/x_Header_Controller.java
@@ -0,0 +1,20 @@
+package springboot.ToDo.Controller;
+
+
+//@Controller
+//@RequestMapping(value = "/")
+//@Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+//@SessionAttributes({"uid_email", "pass", "totally"})  // when you want to store a value in whole session, use this.
+// you have to pass this values from frontend variable standpoint, so it is <uid_email> not <usernr>
+// <usernr> is backend variable, this will not work
+// "uid_email" is the frontend variable, passing this will be able to save as session. it will work..
+public class x_Header_Controller<T> {
+
+
+
+}
+
+
+
+
+
